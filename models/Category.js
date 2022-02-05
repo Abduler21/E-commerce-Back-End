b7850587f73a0de121e3079d01f625ab3cbee779
Abdulmelik Ersoy <kellemen23@gmail.com>
2022-02-04 20:36:26 -0500

    added Category,product,and ProductTag file code

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
+// model to include
+// ID
+// Int
+// no null
+// primary key
+// auto inc
+// category_name
+// string
+// doesnt allow null value
+
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //define category name
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
